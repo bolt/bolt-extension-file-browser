52f2c418748d8245614df692b123a26c2ada4bcc
Gawain Lynch <gawain.lynch@gmail.com>
2016-06-30 13:43:27 +0200

    Extension loader class

---
+++ src/DirectoryIndexExtension.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Bolt\Extension\Bolt\DirectoryIndex;
+
+use Bolt\Extension\SimpleExtension;
+
+/**
+ * Directory index extension loader.
+ *
+ * @author Gawain Lynch <gawain.lynch@gmail.com>
+ */
+class DirectoryIndexExtension extends SimpleExtension
+{
+}
