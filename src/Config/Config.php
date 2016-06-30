98be4f5a68d89a7b6cf4743dc509a0eeeebd4106
Gawain Lynch <gawain.lynch@gmail.com>
2016-06-30 16:44:00 +0200

    Extend abstract and build route objects on initialise()

--- src/Config/Config.php
+++ src/Config/Config.php
@@ -2,13 +2,50 @@
 
 namespace Bolt\Extension\Bolt\DirectoryIndex\Config;
 
-use Symfony\Component\HttpFoundation\ParameterBag;
-
 /**
- * Configuration class.
+ * Main configuration class.
  *
  * @author Gawain Lynch <gawain.lynch@gmail.com>
  */
-class Config extends ParameterBag
+class Config extends AbstractConfig
 {
+    /**
+     * Constructor.
+     *
+     * @param array $parameters
+     */
+    public function __construct(array $parameters = [])
+    {
+        parent::__construct($parameters);
+    }
+
+    /**
+     * @param string $route
+     *
+     * @return Route
+     */
+    public function getRoute($route)
+    {
+        return $this->get('routes')->get($route);
+    }
+
+    /**
+     * @return Routes
+     */
+    public function getRoutes()
+    {
+        return $this->get('routes');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function initialise()
+    {
+        if ($this->initialised === true) {
+            return;
+        }
+        $this->set('routes', new Routes($this->parameters['routes']));
+        parent::initialise();
+    }
 }
